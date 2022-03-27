3abaf1732535f0b36e3ee1b5493bf2afd3a02484
Kruti2673 <krutivadaliya01@gmail.com>
2022-03-27 11:42:13 +0530

    fghdfh

---
+++ TestApp/TestApp/TestApp.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<string> v;
+    v.push_back("Gopi");
+    cout << "Hello World!\n";
+    cout << v.front() << endl;
+}
