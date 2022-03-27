34c974e22887d3bbcd9d78c948c3678cc9bdcee5
Kruti2673 <krutivadaliya01@gmail.com>
2022-03-27 11:45:20 +0530

    new program added

---
+++ GopiTest/GopiTest/GopiTest.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<string> v;
+    v.push_back("Kishan");
+    cout << "Hello World!\n";
+    cout << v.front() << endl;
+}
