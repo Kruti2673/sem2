94a6a1a840bca23ade71cb076814765889f66961
Kruti2673 <krutivadaliya01@gmail.com>
2022-04-30 18:45:33 +0530

    clg program

---
+++ swap value/swap value.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+void swapbyvaule(int a, int b);
+void swapbyreference(int& a, int& b);
+int main()
+{
+	cout << "Kruti Vadaliya" << endl;
+	cout << "92100103349" << endl;
+	int a, b;
+	cout << "Enter First Number:";
+	cin >> a;
+	cout << "Enter Second Number:";
+	cin >> b;
+	swapbyvaule(a, b);
+	cout << "After Swapping:" << endl;
+	cout << "a=" << a << endl;
+	cout << "b=" << b << endl;
+	swapbyreference(a, b);
+	cout << "After Swapping:" << endl;
+	cout << "a=" << a << endl;
+	cout << "b=" << b << endl;
+}
+
+void swapbyvaule(int x, int y)
+{
+	int c = x;
+	x = y;
+	y = c;
+}
+
+void swapbyreference(int& x, int& y)
+{
+	int c = x;
+	x = y;
+	y = c;
+}
