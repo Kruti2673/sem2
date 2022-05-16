c66f3f75efa561a40cec3526bb7d947b73e23cf1
Kruti2673 <krutivadaliya01@gmail.com>
2022-05-16 11:01:48 +0530

    clg program

---
+++ volume and area/volume and area.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+float areaofsphere(int r);
+float volumeofsphere(int r);
+int main()
+{
+	int r;
+	cout << "Enter redius:";
+	cin >> r;
+	cout << "area of sphere:" << areaofsphere(r) << endl;
+	cout << "volume of sphere:" << volumeofsphere(r) << endl;
+	return 0;
+}
+float areaofsphere(int r)
+{
+	return 4 * 3.14 * r * r;
+}
+float volumeofsphere(int r)
+{
+	return 1.33 * 3.14 * r * r * r;
+}
