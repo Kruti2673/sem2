6e4f65e241509ef7036bcdf87dda9e8726403dad
Kruti2673 <krutivadaliya01@gmail.com>
2022-05-17 23:22:46 +0530

    clg program

---
+++ complex num/complex num.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+class Complex
+{
+	int real;
+	int imaginary;
+public:
+	void getComplexNumber()
+	{
+		cout << "Enter real:";
+		cin >> real;
+		cout << "Enter imaginary:";
+		cin >> imaginary;
+	}
+	void displayComplexNumber()
+	{
+		cout << real << "+" << imaginary << "i"<<endl;
+	}
+	Complex()
+	{}
+	Complex(int r, int i)
+	{
+		real = r;
+		imaginary = i;
+	}
+	Complex add(Complex x2)
+	{
+		Complex temp;
+		temp.real = real + x2.real;
+		temp.imaginary = imaginary + x2.imaginary;
+		return temp;
+	}
+};
+
+int main()
+{
+	Complex c1, c2, c3;
+	c1.getComplexNumber();
+	c1.displayComplexNumber();
+	c2.getComplexNumber();
+	c2.displayComplexNumber();
+
+	c3 = c1.add(c2);
+	cout << "addition of 2 complex number:";
+	c3.displayComplexNumber();
+	return 0;
+
+}
